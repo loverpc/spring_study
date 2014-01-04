c066fb55570e5fac7139e363704eb4df0bc6a9e8
loverpc <loverpc@naver.com>
2014-01-04 10:13:53 +0900

    SimpleConnectionMaker

---
+++ chapter1/src/com/loverpc/spring/chapter1/SimpleConnectionMaker.java
@@ -0,0 +1,15 @@
+package com.loverpc.spring.chapter1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+	public Connection makeNewConnection() throws ClassNotFoundException, SQLException
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection( "jdbc:mysql://localhost/spring_test","root","canine99");
+		
+		return c;
+	}
+}
